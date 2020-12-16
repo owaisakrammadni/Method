6dbbd4842367a016b74c593d1d52aa668ddda329
ramshanadeem <rimshanadeem64@gmail.com>
2020-12-16 17:34:52 +0500

    method

---
+++ app.js
@@ -0,0 +1,45 @@
+//sub routine ... par
+
+// function dosomething() {
+
+//     let total = 1800;
+//     let obtained = 500;
+//     let percentage = obtained / total * 100;
+//     console.log(percentage);
+// }
+
+// function dosomething(total, obtained) {
+
+//     let percentage = obtained / total * 100;
+//     console.log(percentage);
+//     document.getElementById(percentage + "<br/>").innerText = percentage;
+// }
+// let total = +prompt("enter total number")
+// let obtained = +prompt("enter t obtained number")
+
+// dosomething(18000, 500)
+//
+// let arr = [];
+// let sum = 0;
+// arr.push(+prompt("enter your value " + (i + 1)))
+// for (let i = 0; i < 10; i++) {
+//     sum =sum arr[i];}
+// function average(arr) {
+//     let aver =sum /arr.length;
+//     return aver;}
+//   let result 
+// function abc (){
+//     console.log ("some code");
+// }
+// var abcFuction = function(){
+//     console.log ("some code");
+// }
+// (function(){
+//     console.log ("some code");
+// })
+// var es6fuction =()=>{
+// console.log ("some code");
+// return"return value"
+// }
+// var es6fuction =()=>"return value"
+// var es6fuction =()=>var1 <var3
